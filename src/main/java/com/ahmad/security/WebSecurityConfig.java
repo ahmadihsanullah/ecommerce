09b74a0fd4adde41d46f7ae02e31f06d17bf10bd
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2025-06-20 15:00:57 +0700

    menambah konfigurasi cors filter untuk mengijinkan react mengakses service

--- src/main/java/com/ahmad/security/WebSecurityConfig.java
+++ src/main/java/com/ahmad/security/WebSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.ahmad.security.jwt.AuthEntryPointJwt;
 import com.ahmad.security.jwt.AuthTokenFilter;
@@ -59,5 +62,22 @@ SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authentication) throws Exception {
         return authentication.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // Ganti dengan origin yang diizinkan
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
